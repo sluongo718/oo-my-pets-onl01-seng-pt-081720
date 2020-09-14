4377a1d275ffc4ae13c2e95d348b2717a5a7fd78
sam l <sluongo718@yahoo.com>
2020-09-14 05:53:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
+require "pry"
 class Owner
   # code goes here
+  attr_reader :name, :species
+  attr_accessor 
+  
+  @@all = []
+  @@count = 0
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self 
+    @@count += 1
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.reset_all
+    @@count = 0
+  end
+  
+  
+  
 end
