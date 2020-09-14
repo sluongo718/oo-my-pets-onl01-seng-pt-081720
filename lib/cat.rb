a4e799984bf0b213fe46755f7298e939dfec36d9
sam l <sluongo718@yahoo.com>
2020-09-14 06:01:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,9 +3,13 @@ class Cat
   
   attr_accessor  :owner, :mood
   attr_reader :name
+  
+  @@all 
+  
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
 end
