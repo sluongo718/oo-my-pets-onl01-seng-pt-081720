be0057eac34805b844c7d504355891c52a69cb2a
sam l <sluongo718@yahoo.com>
2020-09-14 06:08:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,16 @@ class Dog
   attr_accessor  :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner, mood = "nervous")
   @name = name
   @owner = owner
   @mood = mood
+  @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
